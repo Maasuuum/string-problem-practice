d5173d6cb0f43a33f38ab4297e0778d56a4edc91
Maasuuum <fardousalmasum09@gmail.com>
2024-02-28 12:30:23 +0600

    spliting

--- searching.js
+++ searching.js
@@ -1,17 +1,22 @@
 // Search any keywords from multiple words/string
-const lyrics = 'Tumi Bondhu Kala Pakhi, Ami jeno Ki';
+const lyrics = 'Tumi Bondhu Kala Pakhi Ami jeno Ki';
 
 // string is case sensitive. we need to put exact word inside includes otherwise it will show false
 // const doesExist = lyrics.includes('Pakhi');
 // console.log(doesExist);
 
-const searchString = 'pakhi';
-const lyricsLowerCase = lyrics.toLowerCase();
-const sslowercase = searchString.toLowerCase();
-const doesExist = lyricsLowerCase.includes(searchString);
-console.log(doesExist);
+// const searchString = 'pakhi';
+// const lyricsLowerCase = lyrics.toLowerCase();
+// const sslowercase = searchString.toLowerCase();
+// const doesExist = lyricsLowerCase.includes(searchString);
+// const oneLine = lyrics.toLowerCase().includes(searchString.toLowerCase());//All things in one line
+// console.log(oneLine);
+// console.log(doesExist);
 
 // ------------------------
 // search by indexOf
 
 // console.log(lyrics.indexOf('Kala'));
+// ---------------------------
+// startswith
+console.log(lyrics.startsWith('Pakhi'));
---
+++ split-join.js
@@ -0,0 +1,5 @@
+const lyrics = "Tumi bondhu kala pakhi ami jeno ki. Boshonto kale tomay bolate parini. Sada sada kala kala rong jomece sada kala";
+const parts = lyrics.split(' ');
+const sentence = lyrics.split('.');
+console.log(parts);
+console.log(sentence);
