13ce3d2d1a51cfbeb4d5f46a1373032b2147a2a0
Maasuuum <fardousalmasum09@gmail.com>
2024-02-28 12:04:37 +0600

    lower upper

--- searching.js
+++ searching.js
@@ -1,4 +1,17 @@
 // Search any keywords from multiple words/string
 const lyrics = 'Tumi Bondhu Kala Pakhi, Ami jeno Ki';
-const doesExist = lyrics.includes('Pakhi');
+
+// string is case sensitive. we need to put exact word inside includes otherwise it will show false
+// const doesExist = lyrics.includes('Pakhi');
+// console.log(doesExist);
+
+const searchString = 'pakhi';
+const lyricsLowerCase = lyrics.toLowerCase();
+const sslowercase = searchString.toLowerCase();
+const doesExist = lyricsLowerCase.includes(searchString);
 console.log(doesExist);
+
+// ------------------------
+// search by indexOf
+
+// console.log(lyrics.indexOf('Kala'));
