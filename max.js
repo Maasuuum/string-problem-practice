52341f830e44ec544b1807207ef7915a16887d3e
Maasuuum <fardousalmasum09@gmail.com>
2024-02-29 11:08:07 +0600

    minimum maximum

---
+++ max.js
@@ -0,0 +1,28 @@
+const jim = 84;
+const dela = 99;
+const chinku = 77;
+
+// if(jim > dela && jim > chinku){
+//     console.log("Jim will eat the cake");
+// }
+// else if(dela > jim && dela > chinku){
+//     console.log("Dela will eat the cake");
+// }
+// else{
+//     console.log("Chinku will get the cake")
+// }
+
+// -----------------
+// Math.max/Math.min
+const maxNum = Math.max(jim, dela, chinku);
+console.log(maxNum);
+// if(jim === maxNum){
+//     console.log("Jim")
+// }
+// else if(dela === maxNum){
+//     console.log("Dela")
+// }
+// else(chinku === maxNum){
+//     console.log("Chinku")
+// }
+
