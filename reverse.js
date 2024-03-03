ee568370cab9833f26c327cd89e8a6c8b71c2c29
Maasuuum <fardousalmasum09@gmail.com>
2024-03-03 13:54:29 +0600

    reverse string

---
+++ reverse.js
@@ -0,0 +1,8 @@
+function reverseString(text){
+    for(i = 0; i< text.length; i++){
+        const element = text[i];
+        console.log(element);
+    }
+}
+const myString = 'I am a bad boy';
+const reversed = reverseString(myString);
